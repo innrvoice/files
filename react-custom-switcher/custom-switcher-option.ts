70e874fc5456a7302d1bf0c4578087eb5bad2542
Pavel Bochkov-Rastopchin <pbochkov@aligntech.com>
2023-09-28 12:09:36 +0300

    remove obsolete files

--- react-custom-switcher/custom-switcher-option.ts
+++
@@ -1,5 +0,0 @@
-export type CustomSwitcherOption = {
-  value: string;
-  label?: string | React.ReactElement;
-  color?: string;
-};
--- react-custom-switcher/variant.ts
+++
@@ -1,1 +0,0 @@
-export type CustomSwitcherVariant = 'primary' | 'secondary';
